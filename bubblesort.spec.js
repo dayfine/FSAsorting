b2b61e3975035a24bd35e4acd924edd1015a8827
dayfine <difancpa@gmail.com>
2017-07-08 15:50:09 -0400

    bubble sort done

--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,21 +1,26 @@
-describe('Bubble Sort', function(){
+describe('Bubble Sort', function () {
   beforeEach(function () {
-    spyOn(window, 'swap').and.callThrough(); // replace existing `tootsiepop['lick']` method
-  });
+    spyOn(window, 'swap').and.callThrough() // replace existing `tootsiepop['lick']` method
+  })
 
-  it('handles an empty array', function(){
-    expect( bubbleSort([]) ).toEqual( [] );
-  });
+  it('handles an empty array', function () {
+    expect(bubbleSort([])).toEqual([])
+  })
 
-  it('if it has 1 item, return same array', function(){
-    expect( bubbleSort([5]) ).toEqual( [5] );
-  });
+  it('if it has 1 item, return same array', function () {
+    expect(bubbleSort([5])).toEqual([5])
+  })
 
-  it('sorts an array of 2 items correctly', function(){
-    expect( bubbleSort([5, 2]) ).toEqual( [2, 5] );
-  });
+  it('sorts an array of 2 items correctly', function () {
+    expect(bubbleSort([5, 2])).toEqual([2, 5])
+    expect(swap.calls.count()).toEqual(1)
+  })
 
-  it('sorts a large array', function(){
-    expect( bubbleSort([23, 42, 4, 15, 16, 8]) ).toEqual( [4, 8, 15, 16, 23, 42] );
-  });
-});
+  it('sorts a large array', function () {
+    expect(bubbleSort([23, 42, 4, 15, 16, 8])).toEqual([4, 8, 15, 16, 23, 42])
+  })
+
+  it('works for worst peformance case ', function () {
+    expect(bubbleSort([42, 23, 16, 15, 8, 4])).toEqual([4, 8, 15, 16, 23, 42])
+  })
+})
